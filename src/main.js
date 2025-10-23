2a595a33396b32611cb28c050e5fc4f390a0654a
Amanda <rodriguesamanda49@gmail.com>
2025-10-23 16:51:07 -0300

    footer exppanded with paw logo and 2 tabs, different box shadows. ANimal pictures with header and non funct x button

--- src/main.js
+++ src/main.js
@@ -1,43 +1,47 @@
 import "./style.css";
+import "sweetalert2";
 
-const dogBtn = document.querySelector('#dog');
-const catBtn = document.querySelector('#cat');
-const surpriseBtn = document.querySelector('#surprise');
-const text = document.querySelector('#placeholder');
-const img = document.querySelector('#animal-pic');
+const dogBtn = document.querySelector("#dog");
+const catBtn = document.querySelector("#cat");
+const surpriseBtn = document.querySelector("#surprise");
+const dogDiv = document.querySelector("#dog-div");
+const imgDog = document.querySelector("#dog-pic");
+const catDiv = document.querySelector("#cat-div");
+const imgCat = document.querySelector("#cat-pic");
+const foxDiv = document.querySelector("#fox-div");
+const imgFox = document.querySelector("#fox-pic");
+const adultTask = document.querySelector("#adult-task");
 
-dogBtn.addEventListener('dblclick', () => {
-  fetch('https://dog.ceo/api/breeds/image/random')
+dogBtn.addEventListener("click", () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => response.json())
     .then((data) => {
-        const { message } = data;
-        img.style.display = 'inline-block'
-        img.src = message;
-        text.style.display = 'none';
-    })
-})
+      const { message } = data;
+      dogDiv.style.display = "inline-block";
+      imgDog.src = message;
+    });
+});
 
-catBtn.addEventListener('dblclick', () => {
-  fetch('https://api.thecatapi.com/v1/images/search')
+catBtn.addEventListener("click", () => {
+  fetch("https://api.thecatapi.com/v1/images/search")
     .then((response) => response.json())
     .then((data) => {
       const { url } = data[0];
-      img.style.display = 'inline-block'
-      img.src = url;
-      text.style.display = 'none';
-      console.log(url);
-      
-    })
-  })
+      catDiv.style.display = "inline-block";
+      imgCat.src = url;
+    });
+});
 
-surpriseBtn.addEventListener('dblclick', () => {
-  fetch('https://randomfox.ca/floof/?ref=public_apis&utm_medium=website')
-  .then((response) => response.json())
-  .then((data) => {
-    const { image } = data;
-    img.style.display = 'inline-block'  
-    img.src = image;
-    text.style.display = 'none';
-    
+surpriseBtn.addEventListener("click", () => {
+  fetch("https://randomfox.ca/floof/?ref=public_apis&utm_medium=website")
+    .then((response) => response.json())
+    .then((data) => {
+      const { image } = data;
+      foxDiv.style.display = "inline-block";
+      imgFox.src = image;
+    });
+
+  adultTask.addEventListener("click", () => {
+    alert("You clicked the button!");
   });
 });
